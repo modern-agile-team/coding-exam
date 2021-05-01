1623e2d4927b3fc5b6c539c6109a1a7aaaa41711
유준상 <junsang@yujunsangs-MacBook-Pro.local>
2021-05-01 15:55:18 +0900

    제출 2

--- 4.cpp
+++ 4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -15,7 +16,6 @@ string lowerUpperUpperLower(string s) {
 }
 
 void f(string s) {
-  cout << s << endl;
   if (s.empty() == false && s.at(0) == ' ') s.erase(0, 1);
   if (s.empty()) return;
   for (int i = 1; i < s.size(); i++) {
@@ -24,7 +24,7 @@ void f(string s) {
       if (s.at(j) != s.at(j + i + 1)) break;
     }
     if (j == i + 1) {
-      //cout << lowerUpperUpperLower(s.substr(0, i * 2 + 2)) << ' ';
+      cout << lowerUpperUpperLower(s.substr(0, i * 2 + 2)) << ' ';
       if (s.size() < i * 2 + 2) s.clear();
       else s.erase(0, i * 2 + 2);
       break;
@@ -37,5 +37,18 @@ int main() {
   string input;
   getline(cin, input);
   f(input);
+
+
+  // map<string, int> m;
+  // for (int i = 0; i < input.size(); i++) {
+  //   string s = "";
+  //   for (int j = 0; j < input.size(); j++) {
+  //     s += input.at(j);
+  //     m[s] = 1;
+  //     string half = s;
+  //     half.erase(half.size() / 2);
+  //     if (m[half] == 1) cout << half;
+  //   }
+  // } 
   return 0;
 }
