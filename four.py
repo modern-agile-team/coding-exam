15c5a45fd90f71d23a47bcca0685578e76436fe5
Bsfla <baebeomsu77@gmail.com>
2021-05-01 15:56:22 +0900

    배범수 제출합니다

--- four.py
+++ four.py
@@ -1,19 +1,19 @@
 word =  list(input().split())
 new_word = []
-Result = ""
+result = ""
 for i in word :
     new_word.append(i[:len(i)//2]) #중복되는 단어를 중복을 제거합니다  다만 중복되지 않는 단어까지 잘려나가서 이부분은 해결하지 못했습니다
 
 for j in new_word :
     j = j.capitalize()
-    if Result == "" :
+    if result == "" :
     
-       Result = j 
+       result = j 
         
     else :
     
-        Result = Result+" "+j
-print(Result)
+        result = result+" "+j
+print(result)
 
         
         
