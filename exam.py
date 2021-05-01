2ca488cb2feb9555499f31884827138106f7a49d
seohyunsim <83394348+seohyunsim@users.noreply.github.com>
2021-05-01 16:09:48 +0900

    Delete exam.py

--- exam.py
+++
@@ -1,8 +0,0 @@
-def solution(phone_book):
-    b_list = list(phone_book.values())
-    answer = any(b_list)
-    if answer == False:
-        print("True")
-    else:
-        print("False")
-    return answer
